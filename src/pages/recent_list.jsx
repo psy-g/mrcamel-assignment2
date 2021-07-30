f098ddcc1305b858715ec73d3642ed5a0b587878
psy-g <psykyg@gmail.com>
2021-07-30 19:42:48 +0900

    feat: 상품조회이력

--- src/pages/recent_list.jsx
+++ src/pages/recent_list.jsx
@@ -1,18 +1,31 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
 
 import Layout from "components/layout";
 import Filter from "components/Filter";
 import ModalSortingSelector from "components/modal_sorting_selector";
 
 class RecentList extends Component {
-	render() {
-		return (
-			<Layout>
-				<Filter />
-				<ModalSortingSelector />
-			</Layout>
-		);
-	}
+  render() {
+    return (
+      <Layout>
+        <ListWrap>
+          <ProductListTitle>조회한 상품</ProductListTitle>
+          <Filter />
+          <ModalSortingSelector />
+        </ListWrap>
+      </Layout>
+    );
+  }
 }
 
 export default RecentList;
+
+const ListWrap = styled.div`
+  padding: 3rem 5rem;
+`;
+
+const ProductListTitle = styled.p`
+  font-size: 3rem;
+  font-weight: bold;
+`;
