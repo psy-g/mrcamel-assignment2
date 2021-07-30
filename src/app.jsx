65ad2b144af269b84b7ebf584f5da1cab29536e2
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-07-31 00:47:29 +0900

    feat: recentList WIP

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "styled-components/macro";
 import { GlobalStyle } from "styles/global-style";
 
 import Home from "pages/home";
 import ProductDetailPage from "pages/product_detail_page";
 import RecentList from "pages/recent_list";
-import { theme } from "styles/theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
         <Switch>
@@ -19,7 +16,6 @@ function App() {
           <Route path="/recentList" component={RecentList}/>
         </Switch>
       </Router>
-    </ThemeProvider>
   );
 }
 
