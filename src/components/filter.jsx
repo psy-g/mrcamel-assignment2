21741ee7baa2eb09fa610e64d37e115cce767aaf
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-07-30 23:09:20 +0900

    docs: filtr 컴포넌트 수정

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -4,37 +4,79 @@ import styled from "styled-components/macro";
 class Filter extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      allBtn: true,
+    }
+    this.checkBoxRef = createRef();
   }
 
+  brandFilter = (e) => {
+    const target = e;
+    const getSelected = JSON.parse(localStorage.getItem("selected"));
+
+    // 전체
+    if (target === "전체") {
+      this.setState({ data: getSelected, allBtn: true });
+      for (let i = 1; i < this.checkBoxRef.current.children.length; i++) {
+        this.checkBoxRef.current.children[i].children[0].checked = false;
+      }
+    } else {
+      let checkbox = this.checkBoxRef.current.getElementsByTagName("input");
+      let selectedBrand = [];
+
+      for (let ele of checkbox) {
+        if (ele.checked === true) selectedBrand.push(ele.nextSibling.innerText);
+      }
+
+      if (selectedBrand.length > 0) {
+        let temp = getSelected.filter(
+          (ele) => selectedBrand.indexOf(ele.brand) !== -1
+        );
+        this.setState({ data: temp, allBtn: false });
+      } else {
+        this.setState({ data: getSelected, allBtn: true });
+      }
+    }
+  };
+
+  // 관심 필터
+  interestFilter = () => {
+    const { checkInterest } = this.state;
+
+    // 관심 필터 체크 O
+    if (checkInterest) {
+      this.setState({ checkInterest: false });
+    }
+    // 관심 필터 체크 X
+    else {
+      this.setState({ checkInterest: true });
+    }
+  };
+
+
   render() {
-    const {
-      brand,
-      allBtn,
-      interestFitler,
-      brandFilter,
-      checkBoxRef,
-    } = this.props;
+    const {brand} = this.props;
 
     return (
       <Container>
         <Brand>
           <div>필터: 브랜드</div>
-          <CheckboxContainer ref={checkBoxRef}>
+          <CheckboxContainer ref={this.checkBoxRef}>
             {(brand || []).map((ele, index) =>
               index === 0 ? (
                 <label key={index}>
                   <StyledCheckbox
-                    checked={allBtn}
-                    onChange={() => brandFilter(ele)}
-                  ></StyledCheckbox>
+                    checked={this.allBtn}
+                    onChange={this.brandFilter}
+                  />
                   <span>{ele}</span>
                 </label>
               ) : (
                 <label key={index}>
                   <RemainCheckbox
-                    onChange={brandFilter}
-                    allBtn={allBtn}
-                  ></RemainCheckbox>
+                    onChange={this.brandFilter}
+                    allBtn={this.allBtn}
+                  />
                   <span>{ele}</span>
                 </label>
               )
@@ -44,7 +86,7 @@ class Filter extends Component {
         <Interest>
           <div>필터: 관심</div>
           <CheckboxContainer>
-            <StyledCheckbox onClick={interestFitler}></StyledCheckbox>
+            <StyledCheckbox onClick={this.interestFilter} />
             <span>관심없는 상품 숨기기</span>
           </CheckboxContainer>
         </Interest>
