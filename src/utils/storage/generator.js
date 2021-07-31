943a10de027056b905e8bdcf65f90d6d85347f65
psy-g <psykyg@gmail.com>
2021-07-31 20:04:34 +0900

    feat: 로컬 스토리지(시간만료)

--- src/utils/storage/generator.js
+++ src/utils/storage/generator.js
@@ -1,16 +1,29 @@
 export default class Storage {
-	constructor(title) {
-		this.title = title;
-	};
+  constructor(title) {
+    this.title = title;
+  }
 
-	save = (list = []) => {
-		const obj = {
-			data: list,
-			woong: '',
-		}
-		return localStorage.setItem(this.title, JSON.stringify(obj));
-	};
+  save = (list = []) => {
+    const obj = {
+      data: list,
+      timestamp: new Date().getDay(),
+    };
 
-	load = () => JSON.parse(localStorage.getItem(this.title))?.data;
+    if (!localStorage.getItem(this.title)) {
+      localStorage.setItem(this.title, JSON.stringify(obj));
+    } else {
+      let previousTime = JSON.parse(localStorage.getItem(this.title))?.timestamp;
+      let currentTime = new Date().getDay();
+
+      if (previousTime !== currentTime) {
+        return localStorage.clear();
+      } else {
+        return localStorage.setItem(this.title, JSON.stringify(obj));
+      }
+    }
+  };
+
+  load = () => {
+    return JSON.parse(localStorage.getItem(this.title))?.data;
+  };
 }
-
